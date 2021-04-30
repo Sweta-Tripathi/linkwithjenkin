0d74751739a7c8e853d1cbde026f4e8650e73b6c
Sweta Tripathi <82486734+Sweta-Tripathi@users.noreply.github.com>
2021-04-29 18:17:51 -0700

    Add files via upload

---
+++ sum and multiple.py
@@ -0,0 +1,12 @@
+def sumandmultiply(a,b,c):
+
+    d = a+b
+    e = c*d
+    print('Altimate result after adding and multiply is ', e)
+print('hello world')
+
+sumandmultiply(1,2,3)
+sumandmultiply(3,6,9)
+sumandmultiply(100,24,232)
+sumandmultiply(-4,4,5)
+
